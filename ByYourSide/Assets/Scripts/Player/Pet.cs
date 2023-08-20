c6ea3e81a36eda6add9dcb122e7a32deb686c453
LucaPalma <103647073@student.swin.edu.au>
2023-08-21 00:05:04 +1000

    Updates to player movement

--- ByYourSide/Assets/Scripts/Player/Pet.cs
+++ ByYourSide/Assets/Scripts/Player/Pet.cs
@@ -8,7 +8,7 @@ public class Pet : MonoBehaviour
 {
     //References
     Player player;
-    Rigidbody rb;
+    public Rigidbody rb;
     MainCam cam;
 
     //Variables needed for mouse
@@ -41,11 +41,19 @@ public class Pet : MonoBehaviour
         var mousePos = new Vector3(worldPosition.x,player.transform.position.y,worldPosition.z);
 
         //Constrain target position to a certain distance from adventurer.
-        var heading = player.rb.position - mousePos;//Get direction from player to mouse.
-        
+        var heading = mousePos - player.rb.position;//Get direction from player to mouse.
+        heading = heading.normalized; //Normalise direction force.
+        targetPos = (heading * floatDist) + player.rb.position; //Find point equal to float distance from player in target direction.
 
-        //Move this from current position to target position.
-        rb.position = Vector3.MoveTowards(rb.position, targetPos, moveSpeed*Time.deltaTime);
+       //Move if distance to target is too high.
+        if (Vector3.Distance(rb.position, targetPos) > 0.5)
+        {
+            rb.position = Vector3.MoveTowards(rb.position, targetPos, moveSpeed * Time.deltaTime); //Move this from current position to target position.
+        }
+
+
+
+
     }
 
     public void getMousePosition()
--- ByYourSide/Assets/Scripts/Player/Player.cs
+++ ByYourSide/Assets/Scripts/Player/Player.cs
@@ -6,17 +6,18 @@ public class Player : MonoBehaviour
 {
     //References
     public Rigidbody rb;
-
+    Pet pet;
 
     [Header("Movement Stats")]
     public float moveSpeed;
     float intentionX;
     float intentionY;
 
+
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
-
+        pet = FindObjectOfType<Pet>();
     }
 
     void Update()
@@ -36,7 +37,12 @@ public class Player : MonoBehaviour
 
     public void handleMovement()
     {
-        rb.velocity = new Vector3 ((moveSpeed * Time.deltaTime * 100) * intentionX, rb.velocity.y, (moveSpeed * Time.deltaTime * 100) * intentionY);
+        //Update playres velocity based on intention.
+        var newVel = new Vector3((moveSpeed * Time.deltaTime * 100) * intentionX, rb.velocity.y, (moveSpeed * Time.deltaTime * 100) * intentionY);
+        rb.velocity = newVel;
+        pet.rb.velocity = newVel;
+
 
     }
+
 }
