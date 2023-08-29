611b10c92aa63c994e118542f3e932382e2af705
R0binicus <102570571+R0binicus@users.noreply.github.com>
2023-08-29 18:05:31 +1000

    Added line of sight pathing
    Enemy AI will now only go to player if it is in line of sight (LOS). If LOS is blocked by an obstacle, the enemy will not go after the player. If the player was in LOS, but then is blocked by an obstacle it will go to the player's last seen location.

--- ByYourSide/Assets/Scripts/Enemies/BaseEnemyOLD.cs
+++ ByYourSide/Assets/Scripts/Enemies/BaseEnemyOLD.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
+//using Pathfinding;
+using System.Runtime.InteropServices.WindowsRuntime;
 
-public class BaseEnemy : MonoBehaviour
+[RequireComponent(typeof(Rigidbody2D))]
+//[RequireComponent(typeof(Seeker))]
+public class BaseEnemyOLD : MonoBehaviour//, IPausable, ITeams
 {
     //Component Declaration
     protected Rigidbody2D rb;
+    //[SerializeField, EnumMask]
+    //protected Team _team;
 
     //Player Information
     protected Transform player;
     protected Vector3 playerPosition;
     protected Vector2 playerPosition2D;
-    protected Vector3 directionToPlayer;
+    protected Vector2 directionToPlayer;
     protected Vector3 lastSeenPosition;
 
     //Object States Declaration
@@ -22,25 +27,36 @@ public class BaseEnemy : MonoBehaviour
     public bool canMove = true;
 
     //Enemy Values
-    //[SerializeField] protected float fireRate;
-    //[SerializeField] protected float moveSpeed;
+    [SerializeField] protected float fireRate;
+    [SerializeField] protected float moveSpeed;
 
     [SerializeField] private LayerMask barrierLayer;
 
-    private NavMeshAgent agent;
+    //Pathfinding Variables
+    //protected Path currentPath;
+    //protected int currentWaypoint = 0;
+    //[SerializeField] float nextWaypointDistance;
 
-    // Start is called before the first frame update
+    //Seeker seeker;
+
+    // Responds to pausing by disabling the enemy completely
+    public void OnPause() => enabled = false;
+    public void OnResume() => enabled = true;
+
+    //public Team team { get => _team; set => _team = value; }
+    
+    //Initialises components
     private void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         player = GameObject.FindGameObjectWithTag("Player").transform;
-        agent = GetComponent<NavMeshAgent>();
+        //seeker = GetComponent<Seeker>();
 
         lastSeenPosition = transform.position;
-        
-        //InvokeRepeating("UpdatePath", 0f, 0.5f);
+
+        InvokeRepeating("UpdatePath", 0f, 0.5f);
     }
 
-    // Update is called once per frame
     protected virtual void Update()
     {
         //Update the player's position as they move
@@ -61,8 +77,6 @@ public class BaseEnemy : MonoBehaviour
         {
             Move();
         }
-
-        
     }
 
     //Updates the player's location and direction from the enemy.
@@ -75,27 +89,16 @@ public class BaseEnemy : MonoBehaviour
 
     public void CanSeePlayer()
     {
-        //RaycastHit2D hitObject = Physics2D.Raycast(transform.position, directionToPlayer.normalized, directionToPlayer.magnitude, barrierLayer);
+        RaycastHit2D hitObject = Physics2D.Raycast(transform.position, directionToPlayer.normalized, directionToPlayer.magnitude, barrierLayer);
 
-        var ray = new Ray(transform.position, directionToPlayer);
-        RaycastHit hit;
-        Debug.DrawRay(transform.position, directionToPlayer, Color.red);
-        if (Physics.Raycast(ray, out hit, directionToPlayer.magnitude, barrierLayer) && !(hit.transform.gameObject.layer == 9))
+        playerInLOS = hitObject.collider == null;
+
+        // If the player is detected, the corresponding object state is updated.
+        if (playerInLOS)
         {
             seenPlayer = true;
             lastSeenPosition = playerPosition;
         }
-
-        //RaycastHit hitObject = Physics.Raycast(transform.position, directionToPlayer.normalized, directionToPlayer.magnitude, barrierLayer);
-
-        //playerInLOS = hitObject.collider == null;
-//
-        //// If the player is detected, the corresponding object state is updated.
-        //if (playerInLOS)
-        //{
-        //    seenPlayer = true;
-        //    lastSeenPosition = playerPosition;
-        //}
     }
 
     //Fires a projectile at the player if the player is in line of sight.
@@ -109,10 +112,28 @@ public class BaseEnemy : MonoBehaviour
         canAttack = true;
     }
 
+    //Base enemy pathfinding
+    //Creates a new path to the player
+    public void GetPath()
+    {
+        if (canMove)
+        {
+            //seeker.StartPath(rb.position, playerPosition, OnPathComplete);
+        }
+    }
+
+    //Updates the existing path to the player's new location
+    public void UpdatePath()
+    {
+        //if (seeker.IsDone())
+        //{
+        //    GetPath();
+        //}
+    }
+
     //Attempts to move the enemy towards the player
     public void Move()
     {
-        agent.destination = lastSeenPosition;
         ////If no path exists or the enemy cannot move, exit the function
         //if (currentPath == null || canMove == false)
         //{
@@ -124,7 +145,7 @@ public class BaseEnemy : MonoBehaviour
         //{
         //    return;
         //}
-
+//
         ////Calculates the direction and force in which to move the enemy
         //Vector2 direction = ((Vector2)currentPath.vectorPath[currentWaypoint] - rb.position).normalized;
         //Vector2 force = direction * moveSpeed;
@@ -138,4 +159,14 @@ public class BaseEnemy : MonoBehaviour
         //    currentWaypoint++;
         //}
     }
+
+    //After the path has been calculated, applies it to the enemy
+    //public void OnPathComplete(Path p)
+    //{
+    //    if (!p.error)
+    //    {
+    //        currentPath = p;
+    //        currentWaypoint = 0;
+    //    }
+    //}
 }
