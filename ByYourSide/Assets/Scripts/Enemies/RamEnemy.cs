116462a8b145dcae4934fae3077b29fcec819d12
R0binicus <102570571+R0binicus@users.noreply.github.com>
2023-09-17 19:49:47 +1000

    Ram enemy complete
    not quite happy with it, but i don't know how to make it better

---
+++ ByYourSide/Assets/Scripts/Enemies/RamEnemy.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class RamEnemy : BaseEnemy
+{
+    [SerializeField] private float minDistToPlayer = 1f;
+    [SerializeField] private float maxDistToPlayer = 1f;
+    [SerializeField] protected float dmg = 3;
+    [SerializeField] private float pwr = 5;
+
+    //protected iFrameHealth plrHealth;
+    //public GameObject hurtBox;
+
+    //[SerializeField] private string soundName;
+	//private AudioSource abilitySound;
+    //[SerializeField] private string deathName;
+	//private AudioSource deathSound;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+        moveSpeed = agent.speed;
+
+        lastSeenPosition = transform.position;
+
+        //plrHealth = GameObject.FindGameObjectWithTag("Player").GetComponent<iFrameHealth>();
+        //abilitySound = GameObject.Find(soundName).GetComponent<AudioSource>();
+        //deathSound = GameObject.Find(deathName).GetComponent<AudioSource>();
+    }
+    
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        GetPlayerLocation(); //See BaseEnemy parent code
+
+        CanSeePlayer();     //See BaseEnemy parent code
+
+        if (playerInLOS)
+        {
+            if (GetDistanceToPlayer() < maxDistToPlayer && canAttack && !(GetDistanceToPlayer() < minDistToPlayer))
+            {
+                StartCoroutine(Attack());
+            }
+        }
+
+        if (canMove)
+        {
+            Move();
+        }
+    }
+
+    public override IEnumerator Attack()
+    {
+        //Debug.Log("Pew");
+        canMove = false;
+        agent.ResetPath();
+        canAttack = false;
+        this.rb.velocity = Vector3.zero;
+        yield return new WaitForSeconds(0.4f);
+        Vector3 dir = directionToPlayer;
+        yield return new WaitForSeconds(0.1f);
+        //hurtBox.GetComponent<BoxCollider2D>().enabled = true;
+        dir.Normalize();
+        var force = dir * pwr * directionToPlayer.magnitude;
+        this.rb.AddForce(force, ForceMode.Impulse);
+        //abilitySound.Play();
+        yield return new WaitForSeconds(fireRate / 2);
+        canMove = true;
+        yield return new WaitForSeconds(2 / 2);
+        //hurtBox.GetComponent<BoxCollider2D>().enabled = false;
+        canAttack = true;
+    }
+
+    private float GetDistanceToPlayer()
+    {
+        return Vector3.Distance(lastSeenPosition, transform.position);
+    }
+
+    public void DeathSound()
+    {
+        //deathSound.Play();
+    }
+}
