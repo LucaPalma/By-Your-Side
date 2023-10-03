25d7830fd2206c9d20eb20a74323130263707482
R0binicus <102570571+R0binicus@users.noreply.github.com>
2023-10-04 10:54:11 +1100

    Added temp startmenu and scene changes
    press esc to go to start menu

---
+++ ByYourSide/Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public sealed class GameManager : MonoBehaviour
+{
+    private void Awake()
+    {
+
+    }
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            SceneManager.LoadScene("StartMenu");
+
+        }
+        //else if (Input.GetKeyDown()) {
+        //    SceneManager.LoadScene("");
+        //}
+
+    }
+}
